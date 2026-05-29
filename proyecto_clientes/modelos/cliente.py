677900da84ea59b54e42b13819f19bd6d7a814c1
Juan Sebastian Calderon <calderonpenajuansebastian@gmail.com>
2026-05-28 20:34:23 -0500

    Primer commit proyecto FastAPI

---
+++ proyecto_clientes/modelos/cliente.py
@@ -0,0 +1,29 @@
+
+from pydantic import BaseModel
+
+
+class Clientebase (BaseModel):
+    nombre: str
+    edad: int
+    descripcion: str | None=None
+    
+    
+class Clientecrear (Clientebase):
+    pass
+
+class Cliente(Clientebase):
+    id: int | None = None
+    
+
+
+class Facturas(BaseModel):
+    id: int
+    fecha: str
+    cliente: str
+    valortotal: float
+
+
+class Transaccion(BaseModel):
+    id: int
+    descripcion: str
+    factura: int
