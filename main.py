bc0e7d0903472a6336a5b125936eaed0eeeb6cb3
sxl31 <camil8.ce8@gmail.com>
2026-06-18 00:04:26 -0500

    cree el modelo clientes y endpoints listar todos y uno, y crear clientes

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel 
 
 app = FastAPI()
 
-app.get("/")
-def inicio():
-    return {"mensaje": "Hola"} 
+#Crear el modelo clientes (id, nombre, email, descripcion)
+class Cliente(BaseModel):
+    id: int
+    nombre: str
+    email: str
+    descripcion: str
+
+
+lista_clientes:list[Cliente] = []
+
+
+# endpoint para listar todos los clientes
+@app.get("/clientes")
+def listar_clientes():
+    return lista_clientes
+
+# endpoint para listar un solo cliente
+@app.get("/clientes/{cliente_id}")
+def listar_clientes(cliente_id: int):
+    #recorrer la lista clientes
+    for i, obj_cliente in enumerate(lista_clientes):
+        if obj_cliente.get("id") == cliente_id:
+            return obj_cliente
+    return obj_cliente
+
+
+
+# endpoint para crear un solo cliente y agregar a lista
+@app.post("/clientes")
+def crear_cliente(datos_cliente: Cliente):
+   lista_clientes.append(datos_cliente)
+   return datos_cliente
