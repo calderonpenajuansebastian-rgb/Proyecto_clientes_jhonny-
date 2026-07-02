407af2e3193d595a7fa178cb4e577893035af26e
danielriano05 <danielriano05@gmail.com>
2026-07-02 08:04:39 -0500

    Crear estructura inicial del proyecto

--- app/modelos/transacciones.py
+++ app/modelos/transacciones.py
@@ -3,12 +3,14 @@ from pydantic import BaseModel
 class TransaccionBase(BaseModel):
     cantidad: int
     vr_unitario: float
-    factura_id: int
 
 
 class TransaccionCrear(TransaccionBase):
     pass
 
+class TransaccionEditar(TransaccionBase):
+    pass
 
 class Transaccion(TransaccionBase):
     id: int | None = None 
+    factura_id: int | None = None
